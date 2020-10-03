8cbdc7334180a0d5ab7340ce0109c10fc0cddb83
Rashmith <rashmiths.191cs242@nitk.edu.in>
2020-10-03 14:43:56 +0530

    designed each card of building page along with adding detailpages

---
+++ pocket_nitk/lib/screens/hostel_page.dart
@@ -0,0 +1,12 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class HostelPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
+
