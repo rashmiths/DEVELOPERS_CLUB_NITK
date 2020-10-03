5c084cc3a46e05568af64b633b91377afc373289
Rashmith <rashmiths.191cs242@nitk.edu.in>
2020-10-03 14:26:35 +0530

    created objects for building page

---
+++ pocket_nitk/lib/providers/hostel.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+
+class Hostel {
+  final String title;
+  final String imgUrl;
+  final int strength;
+  final int rooms;
+  final String hostelName;
+  final String wardenName;
+  final String monday;
+  final String tuesday;
+  final String wednesday;
+  final String thursday;
+  final String friday;
+  final String saturday;
+  final String sunday;
+
+  Hostel({
+    @required this.title,
+    @required this.imgUrl,
+    @required this.strength,
+    @required this.rooms,
+    @required this.hostelName,
+    @required this.wardenName,
+   @required this.monday,
+   @required this.tuesday,
+   @required this.wednesday,
+   @required this.thursday,
+   @required this.friday,
+    @required this.saturday,
+    @required this.sunday,
+  });
+}
---
+++ pocket_nitk/lib/providers/lecture_hall.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/foundation.dart';
+
+class LectureHall {
+  final String title;
+  final String imgUrl;
+
+  LectureHall({
+   @required this.title,
+    @required this.imgUrl,
+  });
+}
