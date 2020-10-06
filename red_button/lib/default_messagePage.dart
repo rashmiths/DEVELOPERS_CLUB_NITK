cb815761103f33183acbb08cd2431b4475e76d8f
Rashmith <rashmiths.191cs242@nitk.edu.in>
2020-10-07 01:13:11 +0530

    added a dummy message change page

---
+++ red_button/lib/default_messagePage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DefaultMessage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+          body: Center(
+            child: Text('UNDER CONSTRUCTION'),
+        
+      ),
+    );
+  }
+}
